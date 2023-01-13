9b22e78377dfc05647bbe81103a67a3922664851
mohammed <71414472+mohammedhrima@users.noreply.github.com>
2023-01-13 19:59:57 +0100

    Update my_malloc_free.h

--- my_malloc_free.h
+++ my_malloc_free.h
@@ -17,17 +17,6 @@
 #include "limits.h"
 #include "stdlib.h"
 
-typedef struct s_mal
-{
-	unsigned long address;
-	struct s_mal *next;
-	int is_free;
-} t_mal;
-
-t_mal *new_var(unsigned long address);
-t_mal *last_var(t_mal *lst);
-void add_back(t_mal **var, t_mal *new_var);
-t_mal **my_allocated_list(void);
 void* my_malloc(size_t size);
 void my_free(void *ptr, int free_all);
 
